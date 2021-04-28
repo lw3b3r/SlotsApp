c7073cd3f3e38f98c5a584461a20278c1f2a077b
lw3b3r <daevahush@gmail.com>
2021-04-28 15:25:09 -0400

    Initial Commit

---
+++ slots/slotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotsApp.swift
+//  slots
+//
+//  Created by Lin on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct slotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
